468872e89909944a635676385e35ce50d741966a
danny <dpena1094@users.noreply.github.com>
2017-09-28 17:00:50 -0700

    Added multithreading support; Added signal handler for ctrl-c

--- physiologicaldata/data_gatherer.py
+++ physiologicaldata/data_gatherer.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
+import thread
+import time
 
 from logger import DataLogger
 from device_interface import DeviceInterface
@@ -8,8 +10,11 @@ from device_interface import DeviceInterface
 class DataGatherer(object):
     _COLUMNS = ['emotion', 'bpm', 'gaze-x', 'gaze-y']
 
-    def __init__(self, limit, name):
+    _logging = False
+
+    def __init__(self, limit, rate, name):
         self._limit = limit
+        self._rate = rate
         self.__data_log = DataLogger(
             self._limit,
             columns=self._COLUMNS,
@@ -17,11 +22,26 @@ class DataGatherer(object):
         )
         self.__device_interface = DeviceInterface()
 
+        try:
+            thread.start_new_thread(self.__thread_data, ('Thread-1', self._rate))
+        except Exception as e:
+            print(e)
+
+    def __thread_data(self, thread_name, rate):
+        while True:
+            if self._logging:
+                time.sleep(rate)
+                #print(thread_name)
+                self.__device_interface.get_data(self._data)
+
     def _data(self, data):
         self.__data_log.log(data)
 
-    def check(self):
-        self.__device_interface.get_data(self._data)
+    def start(self):
+        self._logging = True
+
+    def stop(self):
+        self._logging = False
 
     def save(self):
         self.__data_log.save()
--- physiologicaldata/logger_test.py
+++ physiologicaldata/logger_test.py
@@ -3,25 +3,31 @@
 from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
+import signal
+import sys
 import time
 
 from data_gatherer import DataGatherer  
 
+def signal_handler(sig, frame):
+    print('Exiting program.')
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
 def main():
-    data = DataGatherer(1000, 'dan')
+    data = DataGatherer(1000, 0.5, 'dan')
+    data.start()
+
     count = 0
-
-    while True:
+    while count < 10:
+        time.sleep(1)
         count += 1
-        if count > 20:
-            break
 
-        data.check()
-        time.sleep(0.1)
-
+    data.stop()
     data.save()
 
-    print('All data logged.')
+    #print('All data logged.')
 
 if __name__ == '__main__':
     main()
