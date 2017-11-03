9e727d492db38277c173dc1f9555b45d457a1580
danny <dpena1094@users.noreply.github.com>
2017-11-02 21:29:33 -0700

    Added client c# program

---
+++ query_handling/client/HmtRequest/HmtRequest/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace HmtRequest {
+    internal class Program {
+        public static void Main(string[] args) {
+
+//            var post = Encoding.UTF8.GetBytes("{\"status\": false}");
+            
+            const string url = @"http://192.168.1.14:9999/query";
+            var request = (HttpWebRequest) WebRequest.Create(url);
+            request.Method = "Post";
+            request.ContentType = "application/json; charset=UTF-8";
+            request.Accept = "application/json";
+//            request.ContentLength = post.Length;
+
+//            var sendStream = request.GetRequestStream();
+//            sendStream.Write(post, 0, post.Length);
+//            sendStream.Close();
+
+            var response = (HttpWebResponse) request.GetResponse();
+            Console.WriteLine(response.ContentLength);
+            Console.WriteLine(response.ContentType);
+
+            var getStream = response.GetResponseStream();
+            StreamReader readStream = null;
+            if (getStream != null)
+                readStream = new StreamReader(getStream, Encoding.UTF8);
+
+            if (readStream != null) {
+                Console.WriteLine(readStream.ReadToEnd());
+                readStream.Close();
+            }
+            
+            response.Close();
+        }
+    }
+}
